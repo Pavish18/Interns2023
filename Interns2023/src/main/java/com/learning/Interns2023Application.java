b7deee62f99c5fc4f507b810c0da740df74f5219
pavish <pavish@LAPTOP-DJ4KK14D>
2023-02-18 09:28:29 +0530

    First commit.

---
+++ Interns2023/src/main/java/com/learning/Interns2023Application.java
@@ -0,0 +1,13 @@
+package com.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Interns2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Interns2023Application.class, args);
+	}
+
+}
